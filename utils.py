f2349d558da0af7e62dc9e77b7bf464d35a1adce
Aplicaciones GM Reportes <proyectobanamexbesco@gmail.com>
2026-06-06 23:00:14 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import gspread
-from google.oauth2.service_account import Credentials
 import smtplib
 from email.message import EmailMessage
 from fpdf import FPDF
@@ -11,21 +10,8 @@ import google.generativeai as genai
 
 # --- CONEXIÓN GOOGLE SHEETS ---
 def obtener_gspread_client():
-    creds_dict = {
-        "type": st.secrets["gcp_service_account"]["type"],
-        "project_id": st.secrets["gcp_service_account"]["project_id"],
-        "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
-        "private_key": st.secrets["gcp_service_account"]["private_key"],
-        "client_email": st.secrets["gcp_service_account"]["client_email"],
-        "client_id": st.secrets["gcp_service_account"]["client_id"],
-        "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
-        "token_uri": st.secrets["gcp_service_account"]["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"]
-    }
-    scopes = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-    credentials = Credentials.from_service_account_info(creds_dict, scopes=scopes)
-    return gspread.authorize(credentials)
+    # Utilizamos el método nativo y más directo de gspread para conectarnos
+    return gspread.service_account_from_dict(dict(st.secrets["gcp_service_account"]))
 
 # --- ENVÍO DE CORREOS AUTOMATIZADO ---
 def enviar_correo(pdf_bytes, cliente, folio, nombre_archivo, corr_extra, destinatarios):
